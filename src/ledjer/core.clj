f3717b1d856044dd51577d28584dbbec4187a4e2
FlorianGit <f.v.kluck@gmail.com>
2021-05-05 12:20:04 +0200

    Move functions in parser namespace to core
    I read in a discussion on the Clojureverse that a recommended practice
    is to just use 1 namespace until a clear separation starts to appear.

--- src/ledjer/core.clj
+++ src/ledjer/core.clj
@@ -1,9 +1,62 @@
 (ns ledjer.core
   (:require [clojure.algo.generic.functor :refer [fmap]]
+            [clojure.edn :as edn]
             [clojure.string :as string]
-            [ledjer.parser :as parser]
             [lentes.core :as l]
             [java-time :refer [local-date-time local-date as truncate-to]]))
+
+(defn add-transaction [journal t]
+  (update journal :transactions conj t))
+
+(defn add-posting [journal p]
+  "Add posting p to last transaction of journal. Throws exception if there is no last transaction."
+  (let [transactions (:transactions journal)
+        last-transaction (peek transactions)
+        new-transaction (update last-transaction :postings conj p)]
+        (assoc journal :transactions (conj (pop transactions) new-transaction))))
+
+
+(defn parse-include-line [line]
+  (if-let [[_ file-name] (re-matches #"include (\S+)" line)]
+    {:include file-name}))
+
+(defn parse-commodity-line [line]
+  (if-let [[_ fmt] (re-matches #"commodity (.*)" line)]
+    {:format fmt}))
+
+(defn parse-transaction-header [x]
+  (if-let [[_ date description]
+        (re-matches #"(\d\d\d\d/\d\d/\d\d) (.*)" x)]
+    {:date (local-date "yyyy/MM/dd" date)
+     :description description}))
+
+(defn parse-posting [x]
+  (if-let [[_ account amount]
+        (re-matches #"\s*([\S:]+)\s+(.*) EUR" x)]
+    {:account account :amount (bigdec (edn/read-string amount))}))
+
+(defn parse-account-name [x]
+  (if-let [matches (re-seq #"(\w+)" x)]
+    (map #(nth % 1) matches)))
+
+(defn parse-empty-line [x]
+  (if (re-matches #"" x)
+    true
+    false))
+
+(defn reduce-fn [acc line]
+  (if-let [header ((some-fn parse-include-line parse-commodity-line) line)]
+    (update acc :headers conj header)
+    (if-let [transaction-header (parse-transaction-header line)]
+      (add-transaction acc transaction-header)
+      (if-let [posting (parse-posting line)]
+        (add-posting acc posting)
+        acc))))
+
+(defn read-ledger-file [contents]
+  (->> contents
+       (string/split-lines)
+       (reduce reduce-fn {:headers [] :transactions []})))
 
 (defn accounts [journal]
   (->> (:transactions journal)
@@ -21,7 +74,7 @@
 (let [transactions 
       (-> "2021.journal"
           (slurp)
-          (parser/read-ledger-file)
+          (read-ledger-file)
           (:transactions))]
   (->> transactions
        (balancesheet)))
@@ -65,7 +118,7 @@
 (def transactions
   (-> "2021.journal"
       (slurp)
-      (parser/read-ledger-file)
+      (read-ledger-file)
       (:transactions)))
 
 (defn account-view [transactions]
